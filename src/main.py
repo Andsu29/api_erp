e6a8d3bea8f5e9632af8f82e5627767555d2aab8
Andsu <andersonduarteg4@gmail.com>
2025-03-03 16:57:18 -0300

    Ajustes de variáveis e permissões para requisições externas com CORS

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from routes.main import app
+from flask_cors import CORS
 
+
+CORS(app)  # Permite todas as origens
+# CORS(app, origins=["http://localhost:5173"])
 
 if __name__ ==  '__main__':
     app.run(host='0.0.0.0', port=8000)
