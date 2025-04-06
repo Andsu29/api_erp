b6f8683fda5858bc45dd43d709d34e6b1f99a789
Andsu <andersonduarteg4@gmail.com>
2025-04-06 17:46:41 -0300

    rota upload adicionada

--- src/routes/main.py
+++ src/routes/main.py
@@ -10,9 +10,35 @@ def all_products():
     return jsonify(products)
 
 @app.route('/inserir_produto', methods=['POST'])
-def insert_product():
+def post_product():
     data = request.json
     if data:
         titulo = data['titulo']
-        methods.post_product(titulo)
-        return jsonify({"Mensagem": "Dados inseridos"}), 200
+        descricao = data['descricao']
+        preco = data['preco']
+        categoria = data['categoria']
+        marca = data['marca']
+        modelo = data['modelo']
+        codpro = data['codpro']
+        print(titulo, descricao, preco, categoria, marca, modelo, codpro)
+        methods.insert_product(titulo, descricao, preco, categoria, marca, modelo, codpro)
+        return jsonify({"Sucesso": "Dados inseridos"}), 200
+    else:
+        return jsonify({'Erro': 'Erro ao adicionar produto'}), 400
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return jsonify({'Erro': 'Campo "file" não encontrado'}), 400
+
+    if 'codPro' not in request.form:
+        return jsonify({'Erro': 'Campo "codPro" não encontrado'}), 400
+    
+    file = request.files['file']
+    codPro = request.form['codPro']
+
+    if file.filename == '':
+        return jsonify({'Erro': 'Nenhum arquivo enviado'}), 400
+
+    methods.insert_image(file.read(), int(codPro))
+    return jsonify({"Mensagem": "Imagem inserida"})
