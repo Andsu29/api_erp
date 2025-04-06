90a3f1cbf71744fee79914732298d61f7c5af52b
Andsu <andersonduarteg4@gmail.com>
2025-04-06 17:45:49 -0300

    Metodo inserir imagem adicionado

--- src/methods/main.py
+++ src/methods/main.py
@@ -1,5 +1,5 @@
 from connection.main import Conn
-from querys.main import query_get_all_products, query_post_products
+from querys.main import query_get_all_products, query_post_products, query_update_file
 
 
 class Methods():
@@ -12,19 +12,31 @@ class Methods():
                 query = query_get_all_products()
                 cursor.execute(query)
                 data = cursor.fetchall()
-                return data
+                if data:
+                    return data
         except Exception as e:
             print(f"Erro ao buscar produtos: {e}")
         finally:
                 self.connection.close_connection()
         
-    def post_product(self, titulo):
+    def insert_product(self, titulo, descricao, preco, categoria, marca, modelo, codpro):
         try:
             with self.connection.create_connection().cursor() as cursor:
-                query = query_post_products(titulo)
+                query = query_post_products(titulo, descricao, preco, categoria, marca, modelo, codpro)
                 cursor.execute(query)
                 self.connection.commit()
         except Exception as e:
             print(f"Erro ao adicionar produto: {e}")
         finally:
-                self.connection.close_connection()
+            self.connection.close_connection()
+
+    def insert_image(self, file, codPro):
+        try:
+            with self.connection.create_connection().cursor() as cursor:
+                query = query_update_file()
+                cursor.execute(query, (file, codPro))
+                self.connection.commit()
+        except Exception as e:
+            print(f"Erro ao adicionar imagem: {e}")
+        finally:
+            self.connection.close_connection()
