a056fbca2ebf3678ce967eeb8a17bd41850498a0
Andsu <andersonduarteg4@gmail.com>
2025-04-06 17:46:26 -0300

    query para inserir imagem adicionada

--- src/querys/main.py
+++ src/querys/main.py
@@ -6,7 +6,10 @@ def query_get_all_products():
     SELECT * FROM produtos
 """
 
-def query_post_products(titulo):
+def query_post_products(titulo, descricao, preco, categoria, marca, modelo, codpro):
+    return f"INSERT INTO produtos (titulo, descricao, preco, categoria, marca, modelo, codpro) VALUES ('{titulo}', '{descricao}', {preco}, '{categoria}', '{marca}', '{modelo}', '{codpro}')"
+
+def query_update_file():
     return f"""
-    INSERT INTO produtos (titulo) VALUES ('{titulo}')
-"""
+    UPDATE produtos SET imagens = %s WHERE codPro = %s;
+    """
